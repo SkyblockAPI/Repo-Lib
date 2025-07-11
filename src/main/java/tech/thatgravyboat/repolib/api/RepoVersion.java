13f4c496205b83108fd3b025ce7426ab34a189a1
Gravy Boat <gravy@thatgravyboat.tech>
2025-07-11 16:30:05 -0230

    chore: fix invalid gradle properties and add method for getting repo version from mc version

--- src/main/java/tech/thatgravyboat/repolib/api/RepoVersion.java
+++ src/main/java/tech/thatgravyboat/repolib/api/RepoVersion.java
@@ -1,5 +1,7 @@
 package tech.thatgravyboat.repolib.api;
 
+import org.jetbrains.annotations.Nullable;
+
 public enum RepoVersion {
     V1_21_4("1_21_4"),
     V1_21_5("1_21_5"),
@@ -15,5 +17,15 @@ public enum RepoVersion {
 
     public String version() {
         return version;
+    }
+
+    public static @Nullable RepoVersion fromName(String name) {
+        name = name.replace(".", "_").replace("-", "_").toUpperCase();
+        for (RepoVersion version : values()) {
+            if (version.version().equals(name)) {
+                return version;
+            }
+        }
+        return null;
     }
 }
