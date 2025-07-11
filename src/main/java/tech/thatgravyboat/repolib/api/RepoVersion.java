41e3f4ae7b406ca4dcab4de1eef65ddc94be5c9b
Gravy Boat <gravy@thatgravyboat.tech>
2025-07-11 17:19:29 -0230

    fix: checking web repo version instead of local repo version

--- src/main/java/tech/thatgravyboat/repolib/api/RepoVersion.java
+++ src/main/java/tech/thatgravyboat/repolib/api/RepoVersion.java
@@ -3,16 +3,18 @@ package tech.thatgravyboat.repolib.api;
 import org.jetbrains.annotations.Nullable;
 
 public enum RepoVersion {
-    V1_21_4("1_21_4"),
-    V1_21_5("1_21_5"),
-    V1_21_6("1_21_5"),
-    V1_21_7("1_21_5"),
+    V1_21_4("1_21_4", "1.21.4"),
+    V1_21_5("1_21_5", "1.21.5"),
+    V1_21_6("1_21_5", "1.21.6"),
+    V1_21_7("1_21_5", "1.21.7"),
     ;
 
     private final String version;
+    private final String mcVersion;
 
-    RepoVersion(String version) {
+    RepoVersion(String version, String mcVersion) {
         this.version = version;
+        this.mcVersion = mcVersion;
     }
 
     public String version() {
@@ -20,9 +22,9 @@ public enum RepoVersion {
     }
 
     public static @Nullable RepoVersion fromName(String name) {
-        name = name.replace(".", "_").replace("-", "_").toUpperCase();
+        name = name.replace("_", ".").replace("-", ".").toUpperCase();
         for (RepoVersion version : values()) {
-            if (version.version().equals(name)) {
+            if (version.mcVersion.equals(name)) {
                 return version;
             }
         }
